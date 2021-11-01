80a6c150960b1b38549bdbb73bcac52f08f5e0e6
pp <me@piotrniewinski.com>
2021-11-01 19:13:38 +0100

    upd

--- demo_nonblocking_more.py
+++ demo_nonblocking_more.py
@@ -26,29 +26,42 @@ def demo_nb(
         ordered_results=    ordered_results,
         verb=               1)
 
-    loop_ix = 0
-    task_ix = 0
-    while True:
-        print(f'\nLoop_ix: {loop_ix}')
+    time.sleep(2) # wait for a while to start workers before loop
 
-        if loop_ix == num_loops: break
+    for lix in range(10):
 
-        n_tasks = 0
-        for _ in range(random.randint(1,10)):
-            task = get_task(task_ix)
-            omp.process(task)
-            task_ix += 1
-            n_tasks += 1
-        print(f' > sent {n_tasks} tasks')
-
-        n_results = 0
+        loop_ix = 0
+        task_ix = 0
+        results_total = 0
         while True:
-            result = omp.get_result(block=False)
-            if result is not None: n_results += 1
-            else: break
-        print(f' > received {n_results} results')
+            print(f'\nLoop_ix: {loop_ix}')
 
-        loop_ix += 1
+            if loop_ix == num_loops: break
+
+            n_tasks = 0
+            for _ in range(random.randint(1,10)):
+                task = get_task(task_ix)
+                omp.process(task)
+                task_ix += 1
+                n_tasks += 1
+            print(f' > sent {n_tasks} tasks')
+
+            n_results = 0
+            while True:
+                result = omp.get_result(block=False)
+                if result is not None: n_results += 1
+                else: break
+            print(f' > received {n_results} results')
+            results_total += n_results
+            loop_ix += 1
+
+        print(f'{num_loops} LOOPS FINISHED, got {task_ix} tasks, received {results_total}, flushing...')
+        while results_total < task_ix:
+            result = omp.get_result(block=False)
+            if result is not None: results_total += 1
+
+        print('done')
+
 
     omp.exit()
 
